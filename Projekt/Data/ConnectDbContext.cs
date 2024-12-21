7d0f1b7ece987c1865fe6cec501a24190f7a51f7
gert Rasmussen <pjente@gmail.com>
2024-12-21 20:16:29 +0100

    Add project files.

---
+++ Projekt/Data/ConnectDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Projekt.Data
+{
+    public class ConnectDbContext : DbContext
+    {
+        public ConnectDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
