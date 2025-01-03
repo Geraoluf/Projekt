67d0c3b4c515262d36d12319b3394a6e8b428197
gert Rasmussen <pjente@gmail.com>
2025-01-03 19:17:18 +0100

    VillaController

---
+++ Projekt/Controllers/VillaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Infrastructure.Data;
+
+
+namespace Projekt.Controllers
+{
+    public class VillaController : Controller
+    {
+
+        private readonly ApplicaationDbContext _db;
+
+        public VillaController(ApplicaationDbContext Context)
+        {
+            _db = Context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var villas =  _db.Villas.ToList();
+            return View(villas);
+        }
+    }
+}
--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<DbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnectionString")));
+builder.Services.AddDbContext<ApplicaationDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
