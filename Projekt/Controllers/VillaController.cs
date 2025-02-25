4663c1cf70d8db26aeac23242dfe726d8a56cafb
gert Rasmussen <pjente@gmail.com>
2025-02-25 23:22:40 +0100

    Villa CRUD

--- Projekt/Controllers/VillaController.cs
+++ Projekt/Controllers/VillaController.cs
@@ -32,11 +32,12 @@ namespace Projekt.Controllers
 
         [HttpPost]
         public IActionResult Create(Villa obj)
-        {
+        {      //custom validation
             if (obj.Name == obj.Description)
             {
                 ModelState.AddModelError("Name", "The description cannot match the name");
             }
+
             if (ModelState.IsValid)
             {
 
@@ -49,6 +50,67 @@ namespace Projekt.Controllers
 
             return View();
         }
+
+        public IActionResult Update(int villaId)
+        {                                           //find element(x), hvor x´s Id er lig med VillaId
+            Villa? obj = _db.Villas.FirstOrDefault(x => x.Id == villaId);
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            
+            if (ModelState.IsValid)
+            {
+
+
+                var ville = _db.Villas.Update(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index", "Villa");
+            }
+
+            return View();
+        }
+
+
+
+        public IActionResult Delete(int villaId)
+        {                                           //find element(x), hvor x´s Id er lig med VillaId
+            Villa? obj = _db.Villas.FirstOrDefault(x => x.Id == villaId);
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {                                          //find element(x), hvor x´s Id er lig med VillaId
+            Villa? objFromDb = _db.Villas.FirstOrDefault(x => x.Id == obj.Id);
+            if (objFromDb is not null)
+            {
+
+
+                var ville = _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index", "Villa");
+            }
+
+            return View();
+        }
+
     }
 
 
