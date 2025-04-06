90d8d04d8653fd5e75dda84a04890cb6701b4765
gert Rasmussen <pjente@gmail.com>
2025-04-06 19:24:38 +0200

    VillaNumber Database

--- WhiteLagoon.Infrastructure/Data/ApplicaationDbContext.cs
+++ WhiteLagoon.Infrastructure/Data/ApplicaationDbContext.cs
@@ -10,29 +10,28 @@ namespace WhiteLagoon.Infrastructure.Data
 {
     public class ApplicaationDbContext : DbContext
     {
-
-        public ApplicaationDbContext(DbContextOptions<ApplicaationDbContext> options) : base(options) 
+        public ApplicaationDbContext(DbContextOptions<ApplicaationDbContext> options) : base(options)
         {
         }
 
         public DbSet<Villa> Villas { get; set; }
-
+        public DbSet<VillaNumber> VillaNumbers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Villa>().HasData(new Villa
-            {
-                Id = 1,
-                Name = "Royal Villa",
-                Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                ImageUrl = "https://placehold.co/600x400",
-                Occupancy = 4,
-                Price = 200,
-                sqft = 550,
-            },
-
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa
+                {
+                    Id = 1,
+                    Name = "Royal Villa",
+                    Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    ImageUrl = "https://placehold.co/600x400",
+                    Occupancy = 4,
+                    Price = 200,
+                    sqft = 550
+                },
                 new Villa
                 {
                     Id = 2,
@@ -41,22 +40,72 @@ namespace WhiteLagoon.Infrastructure.Data
                     ImageUrl = "https://placehold.co/600x401",
                     Occupancy = 4,
                     Price = 300,
-                    sqft = 550,
+                    sqft = 550
                 },
-                    new Villa
-                    {
-                        Id = 3,
-                        Name = "Luxury Pool Villa",
-                        Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                        ImageUrl = "https://placehold.co/600x402",
-                        Occupancy = 4,
-                        Price = 400,
-                        sqft = 750,
-                    });
+                new Villa
+                {
+                    Id = 3,
+                    Name = "Luxury Pool Villa",
+                    Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    ImageUrl = "https://placehold.co/600x402",
+                    Occupancy = 4,
+                    Price = 400,
+                    sqft = 750
+                }
+            );
+
+            modelBuilder.Entity<VillaNumber>().HasData(
+                new VillaNumber
+                {
+                    Villa_Number = 101,
+                    VillaId = 1
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 102,
+                    VillaId = 1
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 103,
+                    VillaId = 1
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 104,
+                    VillaId = 1
+                },
+
+                new VillaNumber
+                {
+                    Villa_Number = 201,
+                    VillaId = 2
+                },
+
+                new VillaNumber
+                {
+                    Villa_Number = 202,
+                    VillaId = 2
+                },
+
+                new VillaNumber
+                {
+                    Villa_Number = 203,
+                    VillaId = 2
+                },
+
+                new VillaNumber
+                {
+                    Villa_Number = 301,
+                    VillaId = 3
+                },
+
+                new VillaNumber
+                {
+                    Villa_Number = 302,
+                    VillaId = 3
+                }
+            );
         }
-
     }
-
-
-
 }
--- WhiteLagoon.Infrastructure/Migrations/20250406172220_VillaNumber.Designer.cs
+++ WhiteLagoon.Infrastructure/Migrations/20250406172220_VillaNumber.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WhiteLagoon.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using WhiteLagoon.Infrastructure.Data;
 namespace WhiteLagoon.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicaationDbContext))]
-    partial class ApplicaationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250406172220_VillaNumber")]
+    partial class VillaNumber
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
